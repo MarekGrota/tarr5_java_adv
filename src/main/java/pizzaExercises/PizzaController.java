66e2192473fa54355858d21b313a4189f6a77b81
Marek Grota <marekgro@gmail.com>
2020-07-12 13:48:48 +0200

    add meat pizza with count of ingredients

--- src/main/java/pizzaExercises/PizzaController.java
+++ src/main/java/pizzaExercises/PizzaController.java
@@ -30,13 +30,44 @@ public class PizzaController {
         return null;
     }
 
+    // metoda wypisująca pizze wraz z cenami
+    public void getAllPizzaWithPrices() {
+        Arrays.stream(Pizza.values())
+                .forEach(pizza -> System.out.println(pizza.getName() + " - " + calculatePizzaPrice(pizza) + " PLN"));
+    }
+
+    // metoda zwracająca najdroższą pizzę wegetariańską
+    Pizza findMostExpensiveVegetarian() {
+        return Arrays.stream(Pizza.values())
+                .filter(pizza -> pizza.getIngredients().stream().noneMatch(ingredient -> ingredient.isMeat()))
+                .sorted(Comparator.comparing(this::calculatePizzaPrice).reversed())
+                .findFirst().get();
+
+    }
+    public long countMeatIngredients (Pizza pizza) {
+        return pizza.getIngredients().stream().filter(Ingredient::isMeat).count();
+    }
+
+    // metoda zwracająca same pizzę mięsne, posortowane malejąco po liczbie składników mięsnych.
+    public List<Pizza> iLikeMeat() {
+        return Arrays.stream(Pizza.values())
+                .filter(pizza -> pizza.getIngredients().stream().anyMatch(ingredient -> ingredient.isMeat()))
+                .sorted(Comparator.comparing(this::countMeatIngredients).reversed()).collect(Collectors.toList());
+
+
+    }
 
     public static void main(String[] args) {
         PizzaController pc = new PizzaController();
         System.out.println("\nCena: " + pc.calculatePizzaPrice(Pizza.MARGHERITA));
         System.out.println("\nOstre: ");
         pc.getAllSpice().forEach(System.out::println);
+        pc.getAllPizzaWithPrices();
         System.out.println("\nNajtańsza pizza ostra: ");
         System.out.println(pc.findCheapestSpicy());
+        System.out.println("Najdroższa pizza wegetariańska: ");
+        System.out.println(pc.findMostExpensiveVegetarian());
+        System.out.println("Pizze mięsne z posortowaną liczbą składników: ");
+        pc.iLikeMeat().forEach(pizza -> System.out.println(pizza + " " + pc.countMeatIngredients(pizza)));
     }
 }
