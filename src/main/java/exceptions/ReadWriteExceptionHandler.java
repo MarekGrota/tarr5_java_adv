44b743e675fd027a08afc90b636dc7ee22cbdbaa
Marek Grota <marekgro@gmail.com>
2020-07-18 12:27:51 +0200

    add MyException class

--- src/main/java/exceptions/ReadWriteExceptionHandler.java
+++ src/main/java/exceptions/ReadWriteExceptionHandler.java
@@ -10,9 +10,10 @@ import java.util.Scanner;
 
 public class ReadWriteExceptionHandler {
     private static String path =
-            Paths.get("").toAbsolutePath().toString()+ "\\src\\main\\java\\exceptions\\data.txt";
+            Paths.get("").toAbsolutePath().toString() + "\\src\\main\\java\\exceptions\\data.txt";
     private static File file = new File(path);
-    public static void appendDataToFile(){
+
+    public static void appendDataToFile() {
         FileWriter fileWriter = null;
         while (true) {
             try {
@@ -20,21 +21,28 @@ public class ReadWriteExceptionHandler {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Wprowadź liczbę (Q - wyjście)");
                 String data = scanner.nextLine();
-                if(data.toUpperCase().equals("Q")){
-//                    fileWriter.close();
+                if (data.toUpperCase().equals("Q")) {
                     break;
                 }
-                String s;
                 double number = Double.valueOf(data);
-                fileWriter.append(String.valueOf(number)+"\n");
-//                fileWriter.close();
+                if (number == 0) {
+                    throw new ArithmeticException();   // sztuczne zgłoszenie wyjątku -> musi być obsłużony
+                }
+                if (number == 13) {
+                    throw new MyException();
+                }
+                fileWriter.append(String.valueOf(number) + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
                 System.out.println("Błąd pliku");
             } catch (InputMismatchException | NumberFormatException e) {
                 e.printStackTrace();
                 System.out.println("Błąd typu danych");
-            } finally {
+            } catch (ArithmeticException e) {            // obsługa wyjątku wyrzuconego za pomocą klauzuli throw
+                System.out.println("Wprowadzona liczba nie może być 0.0");
+            } catch (MyException e) {
+                e.printStackTrace();
+            } finally {     // klauzula ta wykonuje się zawsze po bloku try lub dowolnym bloku catch
                 try {
                     fileWriter.close();
                 } catch (IOException e) {
@@ -43,7 +51,8 @@ public class ReadWriteExceptionHandler {
             }
         }
     }
-    public static void readDataFromFile(){
+
+    public static void readDataFromFile() {
         try {
 
             Scanner scanner = new Scanner(file);
