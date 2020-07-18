6f55cb8ecb9e6a5be8978e3e027363dd0bc121a6
Marek Grota <marekgro@gmail.com>
2020-07-18 12:07:51 +0200

    add write and read method with exception service

--- src/main/java/exceptions/ReadWriteExceptionHandler.java
+++ src/main/java/exceptions/ReadWriteExceptionHandler.java
@@ -1,33 +1,62 @@
 package exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReadWriteExceptionHandler {
-
-        private static String path =
-                Paths.get("").toAbsolutePath().toString()+ "\\src\\main\\java\\exceptions\\data.txt";
-        private File file = new File(path);
-
-    public void appendDataToFile() {
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
+    private static String path =
+            Paths.get("").toAbsolutePath().toString()+ "\\src\\main\\java\\exceptions\\data.txt";
+    private static File file = new File(path);
+    public static void appendDataToFile(){
+        FileWriter fileWriter = null;
+        while (true) {
+            try {
+                fileWriter = new FileWriter(file, true);
+                Scanner scanner = new Scanner(System.in);
                 System.out.println("Wprowadź liczbę (Q - wyjście)");
-                double number = scanner.nextDouble();
-                fileWriter.append(String.valueOf(number));
+                String data = scanner.nextLine();
+                if(data.toUpperCase().equals("Q")){
+//                    fileWriter.close();
+                    break;
+                }
+                String s;
+                double number = Double.valueOf(data);
+                fileWriter.append(String.valueOf(number)+"\n");
+//                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Błąd pliku");
+            } catch (InputMismatchException | NumberFormatException e) {
+                e.printStackTrace();
+                System.out.println("Błąd typu danych");
+            } finally {
+                try {
+                    fileWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+        }
+    }
+    public static void readDataFromFile(){
+        try {
 
-        } catch (IOException e) {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void readDataToFile() {
-
+    public static void main(String[] args) {
+        appendDataToFile();
+        readDataFromFile();
     }
 }
